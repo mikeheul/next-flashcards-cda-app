cb585a891fd4d26d95865600c5fea467d4f7703f
M-Laetitia <molin.laetitia@gmail.com>
2025-01-17 14:53:04 +0100

    Feat: add GET API route for fetching learned flashcards by user ID

---
+++ app/api/flashcard/user/[userId]/learned-flashcards/route.ts
@@ -0,0 +1,38 @@
+import { db } from "@/lib/db";
+import { NextRequest, NextResponse } from "next/server";
+
+type Props = {
+    params: Promise<{ userId: string }>
+}
+
+export async function GET(request: NextRequest, { params }: Props) {
+    try {
+        const { userId } = await params;
+
+        if (!userId) {
+            return NextResponse.json(
+                { error: "User ID is required" }, 
+                { status: 400 }
+            );
+        }
+
+        const learnedCards = await db.learned.findMany({
+            where : {
+                id_user: userId
+            },
+            orderBy: {
+                createdAt: 'desc'
+            },
+        })
+
+        return NextResponse.json({
+            data: learnedCards,
+            message: "Succesfully send the learned cards",
+            success: true, 
+        })
+
+    } catch (error) {
+        console.log("[LEARNED CARDS]", error)
+        return NextResponse.json({ error: "Internal Error2" }, { status: 500 })
+    }
+}
