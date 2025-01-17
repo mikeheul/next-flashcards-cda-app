37b657488aa65a0fcc98432577ef243d706b04c4
M-Laetitia <molin.laetitia@gmail.com>
2025-01-17 14:10:19 +0100

    Feat: add POST API route for creating a new flashcard

--- app/api/flashcard/[flashcardId]/route.ts
+++ app/api/flashcard/[flashcardId]/route.ts
@@ -28,11 +28,48 @@ export async function GET(request: NextRequest, { params }: Props) {
                 { status: 404 } 
             );
         }
-
-        return NextResponse.json(card);
+        // return NextResponse.json(card);
+        return NextResponse.json({
+            data: card, //null si erreur
+            message: "Succesfully send the card", // msg d'erreur si erreur
+            success: true, // false si erreur 
+        })
         
     } catch (error) {
         console.log("[CARD]", error)
         return NextResponse.json({ error: "Internal Error" }, { status: 500 })
     }
+}
+
+export async function POST(request: Request) {
+
+    try {
+        const body = await request.json();
+        const { question, answer, userId} = body;
+
+        if (!question || !answer || !userId) {
+            return NextResponse.json(
+                { error: "Missing required fields" }, 
+                { status: 400 }
+            );
+        }
+        const card = await db.card.create({
+            data: {
+                question,
+                answer,
+                id_user: userId,
+            }
+        });
+        // return NextResponse.json(card, { status: 201 });
+        return NextResponse.json({
+            data: card, 
+            message: " Card succesfully added", 
+            success: true, 
+        })
+
+    } catch (error) {
+        console.log("[CREATE_CARD]", error);
+        return NextResponse.json({ error: "Internal Error" }, { status: 500 });
+    }
+
 }
